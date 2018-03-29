d864caabc702969ad08b8047209879c2eb83d005
Karolin Rafalski <kr2174@caa.columbia.edu>
2018-03-29 12:41:53 -0400

    added partials

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,36 @@
 const app = angular.module('WaterApp', []);
 
 app.controller('MainController', ['$http', function($http) {
-  this.hello = 'hello there'
+  this.hello = 'Water Bar';
+  this.createForm = {};
+  this.waters = [];
+
+  this.includePath = 'partials/menu.html';
+  this.changeInclude = function(path){
+  this.includePath = 'partials/'+ path +'.html';
+}
+
+  this.createWater = ()=>{
+    $http({
+      method: 'POST',
+      url: '/waters',
+      data: this.createForm
+    }).then( response => {
+      console.log(response.data);
+      this.createForm = {};
+    }, error => {
+      console.log(error);
+    })
+  }
+
+  this.getWater = () =>{
+    $http({
+      method: 'GET',
+      url: '/waters'
+    }).then( response => {
+      this.waters = response.data;
+    }, error => console.log( error ))
+  }
+
+  this.getWater();
 }]); // closes app.controller
